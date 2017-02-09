4e017547daef4630f8f1030a76a8b23762e1c01a
Bereket <bereketgebredingle@gmail.com>
2017-02-09 12:18:54 +0300

    404 page is added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTMl>
+<html>
+<head>
+    <title>404</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="404/css/style.css" type="text/css" media="all" />
+
+    <style>
+    #goback {
+    background: url('/404/images/sub-bttn.png');
+    width: 32px;
+    height: 51px;
+    border: none;
+    position: absolute;
+    right: 131px;
+    top: -10px;
+    cursor: pointer;
+    }
+    </style>
+
+</head>
+<body>
+
+<div class="wrap">
+    <h1>Weird Press</h1>
+    <div class="banner"><img src="404/images/fuel-404-logo.png" alt="" /></div>
+    <div class="search">
+        <form>
+            <a href="home" id="goback"></a>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>    
+
